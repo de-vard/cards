d8ed5644823e4c44720cae2cfe7593aefc42e84c
vlad <you@example.com>
2023-07-10 17:08:37 +0300

    10.07.2023

--- lesson/forms.py
+++ lesson/forms.py
@@ -4,7 +4,9 @@ from lesson import models
 
 class LessonCreationForm(forms.ModelForm):
     """Форма создания урока"""
+    title = forms.CharField(label='Название урока')
 
     class Meta:
         model = models.Lesson
+
         fields = ['title', 'word_eng', 'word_rus', 'image', ]
